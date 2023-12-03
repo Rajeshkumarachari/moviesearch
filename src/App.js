7bacc8a5f4a9db312a48c6bb973b655e9374f690
Rajeshkumarachari <131753283+Rajeshkumarachari@users.noreply.github.com>
2023-12-03 09:53:58 +0530

    router done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
+import Body from "./components/Body";
+
 function App() {
-  return (
-    <div className=" text-blue-600 text-center text-2xl">
-      <p>Rajesh</p>
-    </div>
-  );
+  return <Body />;
 }
 
 export default App;
